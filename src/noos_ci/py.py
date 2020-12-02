312afda3063f6386ff6373e72ab086cade60269d
pieeri <eric@rubicubix.co.uk>
2020-12-02 23:18:55 +0000

    Configure python tasks

--- src/noos_ci/py.py
+++ src/noos_ci/py.py
@@ -1,11 +1,17 @@
 import os
 
-from invoke import task
+from invoke import Collection, task
 
 from . import utils
 
 
-PYPI_TOKEN = os.getenv("PYPI_TOKEN")
+CONFIG = {
+    "py": {
+        "target": "./src",
+        "user": "__token__",
+        "token": os.getenv("PYPI_TOKEN"),
+    }
+}
 
 
 # Python deployment workflow
@@ -19,16 +25,18 @@ def clean(ctx):
 
 
 @task
-def format(ctx, target="./src"):
+def format(ctx, target=None):
     """Auto-format source code."""
+    target = target or ctx.py.target
     utils.check_path(target)
     ctx.run(f"black {target}", pty=True)
     ctx.run(f"isort {target}", pty=True)
 
 
 @task
-def lint(ctx, target="./src"):
+def lint(ctx, target=None):
     """Run python linters."""
+    target = target or ctx.py.target
     utils.check_path(target)
     ctx.run(f"black --check {target}", pty=True)
     ctx.run(f"isort --check-only {target}", pty=True)
@@ -38,13 +46,13 @@ def lint(ctx, target="./src"):
 
 
 @task
-def test(ctx, target="./src/tests", group=None):
+def test(ctx, target=None, group=None):
     """Run pytest with optional grouped tests."""
-    path = target
+    path = target or ctx.py.target
     if group:
         path += "/" + group
     utils.check_path(path)
-    ctx.run(f"pytest {target}", pty=True)
+    ctx.run(f"pytest {path}", pty=True)
 
 
 @task
@@ -54,7 +62,19 @@ def package(ctx):
 
 
 @task
-def release(ctx, user="__token__", token=PYPI_TOKEN):
+def release(ctx, user=None, token=None):
     """Publish wheel distribution to PyPi."""
+    user = user or ctx.py.user
+    token = token or ctx.py.token
     assert token is not None, "Missing remote PyPi registry token."
     ctx.run(f"poetry publish --build -u {user} -p {token}", pty=True)
+
+
+ns = Collection("py")
+ns.configure(CONFIG)
+ns.add_task(clean)
+ns.add_task(format)
+ns.add_task(lint)
+ns.add_task(test)
+ns.add_task(package)
+ns.add_task(release)
