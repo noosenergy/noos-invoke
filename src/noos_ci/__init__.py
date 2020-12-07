c90e5da924383d42c5418d943f79544b7a047f22
pieeri <eric@rubicubix.co.uk>
2020-12-07 14:46:01 +0000

    Rename prefix for noos-ci os env var

--- src/noos_ci/__init__.py
+++ src/noos_ci/__init__.py
@@ -1,9 +1,16 @@
 # Using `invoke` as library
 # http://docs.pyinvoke.org/en/stable/concepts/library.html
 
-from invoke import Collection, Program
+from invoke import Collection, Config, Program
 
 from . import dev, docker, git, helm, py
+
+
+__version__ = "0.0.1a3"
+
+
+class BaseConfig(Config):
+    prefix = "noosci"
 
 
 ns = Collection()
@@ -14,4 +21,4 @@ ns.add_collection(helm.ns)
 ns.add_collection(py.ns)
 
 
-main = Program(namespace=ns, version="0.0.1a3")
+main = Program(namespace=ns, config_class=BaseConfig, version=__version__)
