608f1736074b3ac4d260561954caf97fd49fd6dc
pieeri <eric@rubicubix.co.uk>
2020-12-10 16:20:02 +0000

    Bump version to v0.0.2

--- src/noos_ci/__init__.py
+++ src/noos_ci/__init__.py
@@ -6,7 +6,7 @@ from invoke import Collection, Config, Program
 from . import docker, git, helm, local, python, terraform
 
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 
 class BaseConfig(Config):
