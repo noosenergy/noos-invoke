852744e8173197aa99fe6ecc78f5467ea6d1afd9
pieeri <eric@rubicubix.co.uk>
2023-11-28 13:10:09 +0000

    Enforce full typing across terraform commands

--- src/noos_inv/terraform.py
+++ src/noos_inv/terraform.py
@@ -1,5 +1,3 @@
-from typing import Optional
-
 from invoke import Collection, Context, task
 
 
@@ -16,21 +14,34 @@ CONFIG = {
 
 
 @task()
-def update(ctx, variable="", value="", organisation=None, workspace=None, token=None):
+def update(
+    ctx: Context,
+    variable: str = "",
+    value: str = "",
+    organisation: str | None = None,
+    workspace: str | None = None,
+    token: str | None = None,
+) -> None:
     """Update variable in Terraform cloud."""
     cmd = f"noostf update --variable {variable} --value '{value}'"
     ctx.run(cmd + _append_credentials(ctx, organisation, workspace, token), pty=True)
 
 
 @task()
-def run(ctx, message="", organisation=None, workspace=None, token=None):
+def run(
+    ctx,
+    message: str = "",
+    organisation: str | None = None,
+    workspace: str | None = None,
+    token: str | None = None,
+) -> None:
     """Run a plan in Terraform cloud."""
     cmd = f"noostf run --message '{message}'"
     ctx.run(cmd + _append_credentials(ctx, organisation, workspace, token), pty=True)
 
 
 def _append_credentials(
-    ctx: Context, organisation: Optional[str], workspace: Optional[str], token: Optional[str]
+    ctx: Context, organisation: str | None, workspace: str | None, token: str | None
 ) -> str:
     cmd = ""
     for arg in ctx.terraform:
