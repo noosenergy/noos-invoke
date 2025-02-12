b087a9b4f48bf25851318e64d364b752f48135d8
pieeri <eric@rubicubix.co.uk>
2025-02-12 17:03:54 +0100

    Add a basic serializer to check the ports config file schema

--- src/tests/test_utils.py
+++ src/tests/test_utils.py
@@ -10,3 +10,31 @@ class TestCheckPath:
 
     def test_valid_path_passes_silently(self, tmp_path):
         utils.check_path(tmp_path.as_posix())
+
+
+class TestCheckSchema:
+    @pytest.mark.parametrize(
+        "config",
+        [
+            None,
+            ["invalid", "schema"],
+            {"valid": ["invalid", "schema"]},
+            {"valid": {"invalid": "schema"}},
+            {"valid": {"podPrefix": "test-", "podPort": 80, "localPort": 8080}},
+        ],
+    )
+    def test_invalid_schema_raises_error(self, config):
+        with pytest.raises(utils.ValidationError):
+            utils.check_schema(config)
+
+    def test_valid_schema_passes_silently(self):
+        config = {
+            "test": {
+                "podNamespace": "default",
+                "podPrefix": "test-",
+                "podPort": 80,
+                "localPort": 8080,
+            }
+        }
+
+        utils.check_schema(config)
